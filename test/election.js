96561ecddf7a3e195b9b75dabd7814866952a7de
tri <thanhtri7637@gmail.com>
2020-06-18 12:25:11 +0700

    Test case for init 2 candidates

--- test/election.js
+++ test/election.js
@@ -3,5 +3,14 @@ var Election = artifacts.require("./Election.sol");
 contract("Election", function(accounts) {
     var electionInstance;
 
+    it("initializes with two candidates", function() {
+    return Election.deployed().then(function(instance) {
+        return instance.candidatesCount();
+    }).then(function(count) {
+        assert.equal(count, 2);
+    });
+    });
+
     
+
 });
